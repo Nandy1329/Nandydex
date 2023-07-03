84207a0ab845a33b9f5eba8740ff4d6aa4ba6516
Nicholas Andersen <133165079+Nandy1329@users.noreply.github.com>
2023-07-03 11:15:01 -0700

    correcting linting issues found on pages

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-let myName = 'Nicholas';
-document.write(myName);
-
-myName = 'Nicholas';
-document.write(myName);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+alert('Hello World!')
