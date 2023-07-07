fc46e86efe478a189b975f2c16fda199b58b1761
Nicholas Andersen <133165079+Nandy1329@users.noreply.github.com>
2023-07-06 20:58:45 -0700

    Initial creation of html and script.js pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,22 @@
-alert('Hello World!');
-document.write('Meat');
+pokemonList = [
+    {
+        name: Bulbasaur,
+        pokemonNumber: 1,
+        type: ['Grass', 'Poison'],
+    },
+    {
+        name: Machamp,
+        pokemonNumber: 68,
+        type: ['Fighting'],
+    },
+    {
+        name: Gengar,
+        pokemonNumber: 94,
+        type: ['Ghost', 'Poison'],
+    }
+    {
+        name: Magikarp,
+        pokemonNumber: 129,
+        type: ['Water'],
+    }
+];
