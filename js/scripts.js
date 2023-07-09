3b90de16aa730fbccf49c871346ac9649ef9dcea
Nicholas Andersen <133165079+Nandy1329@users.noreply.github.com>
2023-07-09 11:22:00 -0700

    adding loop to pokemonList to check height

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,45 @@
 pokemonList = [
     {
-        name: Bulbasaur,
+        name: "Bulbasaur",
         pokemonNumber: 1,
         type: ['Grass', 'Poison'],
+        height: 2.04
     },
     {
-        name: Machamp,
+        name: "Machamp",
         pokemonNumber: 68,
         type: ['Fighting'],
+        height: 5.03
     },
     {,
-        name: Gengar,
+        name: "Gengar",
         pokemonNumber: 94,
         type: ['Ghost', 'Poison'],
-    }
+        height: 4.11 
+    ,}
     {,
-        name: Magikarp,
+        name: "Magikarp",
         pokemonNumber: 129,
         type: ['Water'],
-    }
-];
+        height: 2.11 
+    },
+]; 
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write (
+        "#" + pokemonList[i].id + " " + pokemonList[i].name + " <br> "
+    ) 
+
+    if (pokemonList[i].height > 4) {
+        document.write(
+            '(Height: ${pokemonList[i].height} ft) - Wow, thats big!'
+            
+            + "<br>"
+        )
+    }else {
+        document.write(
+            'height: ${pokemonList[i].height} ft'
+            
+            + "<br>"
+            
+    }   )
