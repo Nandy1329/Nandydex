c1ea6f704fb7beeaa58dfea4761262f29115a295
Nicholas Andersen <133165079+Nandy1329@users.noreply.github.com>
2023-07-10 14:30:33 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,47 @@
 pokemonList = [
     {
         name: "Pikachu",
-        id, 25,
-        type: ['electric'],
-        height: 1.04
+        id, 25:
+        type, ['electric']:
+        height, 1.04:
+
     },
     {
         name: "Machamp",
-        id, 68,
-        type: ['Fighting'],
-        height: 5.03
+        id, 68:
+        type, ['Fighting']:
+        height, 5.03:
     },
-    {,
-        name, "Gengar",
-        id, 94,
-        type, ['Ghost', 'Poison'],
-        height, 4.11 
-    ,}
-    {,
-        name, "Magikarp",
-        id: 129,
-        type, ['Water'],
-        height, 2.11 
-    },
+    {
+
+        name, "Gengar":
+        id, 94:
+        type, ['Ghost', 'Poison']:
+        height, 4.11 :
+    }
+    {
+        name, "Magikarp":
+        id: 129:
+        type, ['Water']:
+        height, 2.11 : 
+    }
 ]; 
-
-
 for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >=8){
+    document.write( "#" + pokemonList[i].id + " " + pokemonList[i].name + "<br>")"
 
-        document.write(pokemonList[i].name + pokemonList[i].height + "- Woah, that's huge!" + "<br/>");
-    }else if (pokemonList[i].height <8 && pokemonList[i].height >=5){
-        document.write(pokemonList[i].name + pokemonList[i].height + "- This is an average size" + "<br/>");
-    } else {
+    if (pokemonList[i].height >=4) {
+        document.write(
+           '( height ${pokemonList[i].height} ft) - Woah, that's huge!' 
+           + "<br/>");
+
+    }
+    
+    else if (pokemonList[i].height <2.5 && pokemonList[i].height >=2){
+        document.write(${pokemonList[i]:height}+ "- This is an average size" + "<br/>");
+  
+    } 
+   
+    else {
         document.write(pokemonList[i].name + pokemonList[i].height + "- This guy is tiny!" + "<br/>");
     }
     }
