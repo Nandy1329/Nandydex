33ca6814e6660e011e8b72e6f369a73f5691b22b
Nicholas Andersen <133165079+Nandy1329@users.noreply.github.com>
2023-07-10 19:08:45 -0700

    fixing linting mistakes

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,39 @@
+js
 let pokemonList = [
+{ 
+     name: "Lugia",
+     id: 249,
+     type: ["Psychic", "Flying"],
+     height: 1.04
+  },
+  {
+     name: "Machamp",
+     id: 68,
+     type: ["Fighting"],
+     height: 5.03 
+  },    
+  {
+     name: "Gengar",
+     id: 94,
+     type: ["Ghost", "Poison"],
+     height: 4.11 
+  },
+  {
+     name:"Magikarp",
+     id: 129,
+     type: ["Water"],
+     height: 2.11 
+    },
     {
-        name: "Pikachu",
-        id, 25:
-        type, ['electric']:
-        height, 1.04:
+        name: "Wailord",
+        id: 321,
+        type: ["Water"],
+        height: 47.07
+    }
+    ];
 
-
-    {
-        name: "Machamp",
-        id, 68:
-        type, ['Fighting']:
-        height, 5.03:
-    
-    {
-
-        name, "Gengar":
-        id, 94:
-        type, ['Ghost', 'Poison']:
-        height, 4.11 :
-    
-    {
-        name, "Magikarp":
-        id, 129:
-        type, ['Water']:
-        height, 2.11 : 
-    
-
-]; 
-
- for (let i = 0; i < pokemonList.length; i++) {
-    document.write (
-        "#" + pokemonList[i].id + " " + pokemonList[i].name
-        + "<br>"
-    )
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write ("#" + pokemonList[i].id + " " + pokemonList[i].name + "<br>" );
 
     if (pokemonList[i].type.length > 1) {
         document.write(
@@ -40,23 +41,16 @@ let pokemonList = [
             + "<br>"
         )
     } else {
-        document.write(
-           '( height: ${pokemonList[i].height} ft) - Woah, that is huge!' 
-           + "<br>");
-
+        document.write( `(Type: ${pokemonList[i].type})` + "<br>")
     }
 
     if (pokemonList[i].height <= 1) {
         document.write(
-            `(Height: ${pokemonList[i].height} ft) - Wow, that is small!`
-            + "<br>"
-        )
-    } else {
-        document.write(
-            `(Height: ${pokemonList[i].height} ft)`
-            + "<br>"
-        )
-    }
+            `(Height: ${pokemonList[i].height}m) - Wow! This pokemon is tiny!` + "<br>");
+      
+        } else {
+         document.write(`(Height: ${pokemonList[i].height} ft)` + "<br>");
+  }
 
-    document.write("<br>")
-}
+document.write("<br>");
+        }
