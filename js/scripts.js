d2b5422be4b84facbd937b1bd96857e66dc6668e
Nicholas Andersen <133165079+Nandy1329@users.noreply.github.com>
2023-07-10 20:11:49 -0700

    fixed for submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,54 @@
-js
 let pokemonList = [
-{ 
-     name: "Lugia",
-     id: 249,
-     type: ["Psychic", "Flying"],
-     height: 1.04
-  },
-  {
-     name: "Machamp",
-     id: 68,
-     type: ["Fighting"],
-     height: 5.03 
-  },    
-  {
-     name: "Gengar",
-     id: 94,
-     type: ["Ghost", "Poison"],
-     height: 4.11 
-  },
-  {
-     name:"Magikarp",
-     id: 129,
-     type: ["Water"],
-     height: 2.11 
+    {
+      name: "Lugia",
+      id: 249,
+      type: ["Psychic", "Flying"],
+      height: 17.04
     },
     {
-        name: "Wailord",
-        id: 321,
-        type: ["Water"],
-        height: 47.07
+      name: "Machamp",
+      id: 68,
+      type: ["Fighting"],
+      height: 5.03
+    },
+    {
+      name: "Gengar",
+      id: 94,
+      type: ["Ghost", "Poison"],
+      height: 4.11
+    },
+    {
+      name: "Magikarp",
+      id: 129,
+      type: ["Water"],
+      height: 2.11
+    },
+    {
+      name: "Wailord",
+      id: 321,
+      type: ["Water"],
+      height: 47.07
     }
-    ];
-
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write ("#" + pokemonList[i].id + " " + pokemonList[i].name + "<br>" );
-
+  ];
+  
+  for (let i = 0; i < pokemonList.length; i++) {
+    document.write("#" + pokemonList[i].id + " " + pokemonList[i].name + "<br>");
     if (pokemonList[i].type.length > 1) {
-        document.write(
-            `(Types: ${pokemonList[i].type[0]}, ${pokemonList[i].type[1]}) - Wow! This pokemon has two types!`
-            + "<br>"
-        )
+      document.write(
+        `(Types: ${pokemonList[i].type[0]}, ${pokemonList[i].type[1]}) - Wow! This pokemon has two types!` +
+          "<br>"
+      );
     } else {
-        document.write( `(Type: ${pokemonList[i].type})` + "<br>")
+      document.write(`(Type: ${pokemonList[i].type})` + "<br>");
     }
-
-    if (pokemonList[i].height <= 1) {
-        document.write(
-            `(Height: ${pokemonList[i].height}m) - Wow! This pokemon is tiny!` + "<br>");
-      
-        } else {
-         document.write(`(Height: ${pokemonList[i].height} ft)` + "<br>");
+  
+    if (pokemonList[i].height >= 20) {
+      document.write(
+        `(Height: ${pokemonList[i].height} ft) - Wow, thats HUGE!` + "<br>"
+      );
+    } else {
+      document.write(`(Height: ${pokemonList[i].height} ft)` + "<br>");
+    }
+    document.write("<br>");
   }
-
-document.write("<br>");
-        }
+  
