db02bffdeb726d195526109a7ff2b7e3bdae61b2
Nicholas Andersen <133165079+Nandy1329@users.noreply.github.com>
2023-07-18 20:22:21 -0700

    Update to loops and conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,69 @@
-let pokemonList = [
-    {
-      name: "Lugia",
-      id: 249,
-      type: ["Psychic", "Flying"],
-      height: 17.04
-    },
-    {
-      name: "Machamp",
-      id: 68,
-      type: ["Fighting"],
-      height: 5.03
-    },
-    {
-      name: "Gengar",
-      id: 94,
-      type: ["Ghost", "Poison"],
-      height: 4.11
-    },
-    {
-      name: "Magikarp",
-      id: 129,
-      type: ["Water"],
-      height: 2.11
-    },
-    {
-      name: "Wailord",
-      id: 321,
-      type: ["Water"],
-      height: 47.07
-    }
+let pokemonRepository = (function () {
+  let pokemonList = [ 
+  {
+    name: "Lugia",
+    id: 249,
+    typeof: "Psychic, Flying",
+    height: 17.01
+  },
+  {
+    name: "Mewtwo",
+    id: 150,
+    typeof: "Psychic", 
+    height: 6.07
+  },
+{
+
+  name: "Machamp",
+  id: 68,
+  typeof: "Fighting",
+  height: 5.03
+},
+{
+  name: "Gengar",
+  id: 94,
+  typeof: "Ghost, Poison",
+  height: 4.11
+},
+{
+  name: "Magikarp",
+  id: 130,
+  typeof: "Water",
+  height: 2.11
+},
+{
+
+  name: "Gyarados",
+  id: 130,
+  typeof: "Water, Flying",
+  height: 21.04
+},
+{
+  name: "Wailord",
+  id: 321,
+  typeof: "Water",
+  height: 47.07
+},
   ];
-  
-  for (let i = 0; i < pokemonList.length; i++) {
-    document.write("#" + pokemonList[i].id + " " + pokemonList[i].name + "<br>");
-    if (pokemonList[i].type.length > 1) {
-      document.write(
-        `(Types: ${pokemonList[i].type[0]}, ${pokemonList[i].type[1]}) - Wow! This pokemon has two types!` +
-          "<br>"
-      );
-    } else {
-      document.write(`(Type: ${pokemonList[i].type})` + "<br>");
-    }
-  
-    if (pokemonList[i].height >= 20) {
-      document.write(
-        `(Height: ${pokemonList[i].height} ft) - Wow, thats HUGE!` + "<br>"
-      );
-    } else {
-      document.write(`(Height: ${pokemonList[i].height} ft)` + "<br>");
-    }
-    document.write("<br>");
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
+
+
+  return {
+    getAll: getAll,
+    add: add
+
+  };
+})();
+
+let pokemonDetails = pokemon.name + " (height: " + pokemon.height + ")";
+
+if (pokemon.height > 30) {
+  pokemonDetails += " - Wow, that's HUGE!";
+}
+
+  document.write(pokemonDetails + "<br>");
+});
   
